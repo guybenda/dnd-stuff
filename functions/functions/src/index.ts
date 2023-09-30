49a7607a5c0e6e2013701424ac8edc7d825ff839
guybenda <43969637+guybenda@users.noreply.github.com>
2023-09-30 21:31:30 +0300

    Fix common module

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -7,6 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+import { initializeApp } from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 import { HttpsOptions, onRequest } from "firebase-functions/v2/https";
@@ -16,11 +17,14 @@ import { HttpsOptions, onRequest } from "firebase-functions/v2/https";
 
 const OPTIONS: HttpsOptions = { region: "europe-west3" };
 
+initializeApp();
+
 export const helloWorld = onRequest(OPTIONS, (request, response) => {
 	logger.info("Hello logs!", { structuredData: true });
 	response.send("Hello from Firebase!");
 });
 
-export const getSessions = onRequest(OPTIONS, async (request, response) => {
-	getFirestore();
+export const getGames = onRequest(OPTIONS, async (request, response) => {
+	const games = await getFirestore().collection("games").limit(10).get();
+	response.send(games);
 });
