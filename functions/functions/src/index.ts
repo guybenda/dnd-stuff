1a2ddd4740292b3b61198347b1c1c3214d6b633e
guybenda <43969637+guybenda@users.noreply.github.com>
2023-09-30 20:12:57 +0300

    Common module

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -7,13 +7,20 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+import { getFirestore } from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
-import { onRequest } from "firebase-functions/v2/https";
+import { HttpsOptions, onRequest } from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
+const OPTIONS: HttpsOptions = { region: "europe-west3" };
+
+export const helloWorld = onRequest(OPTIONS, (request, response) => {
 	logger.info("Hello logs!", { structuredData: true });
 	response.send("Hello from Firebase!");
+});
+
+export const getSessions = onRequest(OPTIONS, async (request, response) => {
+	getFirestore();
 });
