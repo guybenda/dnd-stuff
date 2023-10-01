62b5e899378e648853b4e46a286b537989faf8e5
guybenda <43969637+guybenda@users.noreply.github.com>
2023-10-01 22:31:25 +0300

    Make TS build bullshit work

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { cert, initializeApp } from "firebase-admin/app";
 
 import { getFirestore } from "firebase-admin/firestore";
-import key from "../key.json";
+import key from "./key.js";
 
 initializeApp({
 	credential: cert(key),
@@ -11,4 +11,4 @@ const db = getFirestore();
 
 const games = await db.collection("games").limit(10).get();
 
-console.log(games);
+games.forEach(game => console.log(game.id, game.data()));
